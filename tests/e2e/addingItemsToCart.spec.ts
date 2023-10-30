c8825a333055bc0289d78901f61d90a27baf7e2f
KastriotBlakaj <lotiblakaj123@gmail.com>
2023-10-30 11:04:23 +0100

    test data added

--- tests/e2e/addingItemsToCart.spec.ts
+++ tests/e2e/addingItemsToCart.spec.ts
@@ -1,24 +1,31 @@
-import {expect, test} from "../helpers/fixture"
+import { expect, test } from "../helpers/fixture";
+import * as data from "../testData/testData.json"
 
-test("Add Item to Cart and checkout ", async({page,loginPageElement,homePageElement,checkoutPageElement,finishPageElement})=>{
+test("Add Item to Cart and checkout ", async ({
+  page,
+  loginPageElement,
+  homePageElement,
+  checkoutPageElement,
+  finishPageElement,
+}) => {
     
-    await page.goto(`https://www.saucedemo.com/`)
-    await loginPageElement.enterFirstName("standard_user")
-    await loginPageElement.enterPassword("secret_sauce")
-    await loginPageElement.clickLogin()
-    expect(await page.title()).toBe("Swag Labs") //This part will be moved into a fixture 
+  await page.goto(`https://www.saucedemo.com/`);
+  await loginPageElement.enterFirstName(data.email);
+  await loginPageElement.enterPassword(data.password);
+  await loginPageElement.clickLogin();
+  expect(await page.title()).toBe("Swag Labs"); //This part will be moved into a fixture
 
-    await homePageElement.verifyHeadlineText
-    await homePageElement.addToCartButton()
-    await homePageElement.clickCartButton()
-    await homePageElement.verifyHeadlineTextInCheckout()
-    await homePageElement.continuetoCheckout()
+  await homePageElement.verifyHeadlineText;
+  await homePageElement.addToCartButton();
+  await homePageElement.clickCartButton();
+  await homePageElement.verifyHeadlineTextInCheckout();
+  await homePageElement.continuetoCheckout();
 
-    await checkoutPageElement.checkoutFirstNameData("test")
-    await checkoutPageElement.checkoutLastNameData("lastname")
-    await checkoutPageElement.checkoutZipCodeData("address1")
-    await checkoutPageElement.checkoutContinueButton()
+  await checkoutPageElement.checkoutFirstNameData(data.checkoutEmail);
+  await checkoutPageElement.checkoutLastNameData(data.checkoutLastName);
+  await checkoutPageElement.checkoutZipCodeData(data.checkoutZip);
+  await checkoutPageElement.checkoutContinueButton();
 
-    await finishPageElement.verifyFinishPageHeadline()
-    await finishPageElement.clickOnFinishButton()
-})
+  await finishPageElement.verifyFinishPageHeadline();
+  await finishPageElement.clickOnFinishButton();
+});
--- tests/pages/loginPage.ts
+++ tests/pages/loginPage.ts
@@ -1,27 +1,20 @@
 import { Page } from "@playwright/test";
 
-export default class loginPage{
+export default class loginPage {
+  constructor(public page: Page) {}
 
-    constructor(public page:Page){
+  async enterFirstName(username: string) {
+    await this.page.locator("input[id='user-name']").type(username);
+  }
 
-    }
+  async enterPassword(password: string) {
+    await this.page.locator("input[id='password']").type(password);
+  }
 
-   async enterFirstName(username: string){
-     await this.page.locator("input[id='user-name']")
-        .type(username)
-    }
-
-    async enterPassword(password: string){
-        await this.page.locator("input[id='password']")
-           .type(password)
-       }
-
-    async clickLogin(){
-        await Promise.all([
-            this.page.waitForNavigation({ waitUntil: "networkidle" }),
-            this.page.click("input[id='login-button']")
-        ])
-    }
-
-    //Verify the item headline
+  async clickLogin() {
+    await Promise.all([
+      this.page.waitForNavigation({ waitUntil: "networkidle" }),
+      this.page.click("input[id='login-button']"),
+    ]);
+  }
 }
