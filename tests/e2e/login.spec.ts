40b408aebabb13c650f190db4a9a3968f83ee204
KastriotBlakaj <lotiblakaj123@gmail.com>
2023-12-26 23:37:49 +0100

    json data used

--- tests/e2e/login.spec.ts
+++ tests/e2e/login.spec.ts
@@ -1,14 +1,14 @@
-import {expect, test} from "@playwright/test"
-import loginPage from "../pages/loginPage"
-import homePage from "../pages/homePage"
-import { log } from "console"
+import { expect, test } from "@playwright/test";
+import loginPage from "../pages/loginPage";
+import homePage from "../pages/homePage";
+import * as data from "../testData/testData.json";
+import { log } from "console";
 
-test("LoginTest", async({page, baseURL})=>{
-    const login = new loginPage(page)
-    await page.goto(`https://www.saucedemo.com/`)
-    await login.enterFirstName("standard_user")
-    await login.enterPassword("secret_sauce")
-    await login.clickLogin()
-    expect(await page.title()).toBe("Swag Labs")
-
-})
+test("LoginTest", async ({ page, baseURL }) => {
+  const login = new loginPage(page);
+  await page.goto(`https://www.saucedemo.com/`);
+  await login.enterFirstName(data.email);
+  await login.enterPassword(data.password);
+  await login.clickLogin();
+  expect(await page.title()).toBe("Swag Labs");
+});
--- tests/pages/homePage.ts
+++ tests/pages/homePage.ts
@@ -1,43 +1,43 @@
-import { Page , Locator} from "@playwright/test";
+import { Page, Locator } from "@playwright/test";
 
+export default class homePage {
+  private readonly headLineTextField: Locator;
+  private readonly addtoCartButton: Locator;
+  private readonly clickToCartButton: Locator;
+  private readonly checkoutVeriftyTextHeadline: Locator;
+  private readonly continueToCheckoutButton: Locator;
 
-export default class homePage{  
+  constructor(public page: Page) {
+    this.headLineTextField = this.page.locator(
+      "#header_container > div.primary_header > div.header_label > div"
+    );
+    this.addtoCartButton = this.page.locator(
+      'button[id="add-to-cart-sauce-labs-backpack"]'
+    );
+    this.clickToCartButton = this.page.locator("#shopping_cart_container > a");
+    this.checkoutVeriftyTextHeadline = this.page.locator(
+      "#cart_contents_container > div > div.cart_list > div.cart_desc_label"
+    );
+    this.continueToCheckoutButton = this.page.locator("#checkout");
+  }
 
-    private readonly headLineTextField: Locator;
-    private readonly addtoCartButton: Locator;
-    private readonly clickToCartButton: Locator;
-    private readonly checkoutVeriftyTextHeadline: Locator;
-    private readonly continueToCheckoutButton: Locator;
+  async verifyHeadlineText() {
+    return this.headLineTextField.waitFor({ state: "visible" });
+  }
 
-    constructor(public page:Page){
-        this.headLineTextField = this.page.locator('#header_container > div.primary_header > div.header_label > div')
-        this.addtoCartButton = this.page.locator('button[id="add-to-cart-sauce-labs-backpack"]')
-        this.clickToCartButton = this.page.locator('#shopping_cart_container > a')
-        this.checkoutVeriftyTextHeadline= this.page.locator('#cart_contents_container > div > div.cart_list > div.cart_desc_label')
-        this.continueToCheckoutButton = this.page.locator('#checkout')
+  async addToCartButton() {
+    await this.addtoCartButton.click();
+  }
 
-    }
+  async clickCartButton() {
+    await this.clickToCartButton.click();
+  }
 
-    async verifyHeadlineText(){
-        return this.headLineTextField
-        .waitFor({state: "visible"})
-    }
+  async verifyHeadlineTextInCheckout() {
+    return this.checkoutVeriftyTextHeadline.waitFor({ state: "visible" });
+  }
 
-    async addToCartButton(){
-        await this.addtoCartButton.click()
-    }
-
-    async clickCartButton(){
-        await this.clickToCartButton.click()
-    }
-
-    async verifyHeadlineTextInCheckout(){
-        return this.checkoutVeriftyTextHeadline
-        .waitFor({state: "visible"})
-    }
-
-    async continuetoCheckout(){
-        await this.continueToCheckoutButton.click()
-    }
-
+  async continuetoCheckout() {
+    await this.continueToCheckoutButton.click();
+  }
 }
